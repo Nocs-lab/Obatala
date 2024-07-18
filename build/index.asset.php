473982f0df6fd264bbfcf2ae45b68d3dc8dc43ea
luciano-alexandre <luciano.cnrn@gmail.com>
2024-07-18 12:18:12 -0300

    Adição de listagem de etapas no Process Viewer

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-element', 'wp-primitives'), 'version' => '8368a6ec5bdf30f79b0d');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-element', 'wp-primitives'), 'version' => '930222200717abfe0740');
