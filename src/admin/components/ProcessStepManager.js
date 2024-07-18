f4ea7fcdad77a972faa6686cb14381e5f9e6252f
Fábio Rocha <fabio.gomes@ifrn.edu.br>
2024-07-17 23:52:01 -0300

    Removing space

--- src/admin/components/ProcessStepManager.js
+++ src/admin/components/ProcessStepManager.js
@@ -1,4 +1,4 @@
-import  React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Spinner, Button, SelectControl, TextControl, Notice, Panel, PanelHeader, PanelBody, PanelRow, Icon, DatePicker, RadioControl } from '@wordpress/components';
 import apiFetch from '@wordpress/api-fetch';
 import { trash } from "@wordpress/icons";
