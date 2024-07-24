3656515d71e9da87ebafd0cd7015dd124164a317
anaclrra <anaclarab826@gmail.com>
2024-07-24 18:49:27 -0300

    refactor: atualiza processTypeList Este commit atualiza o useEffect para inicializar o estado das etapas considerando a ordem das etapas por tipo de processo. Este commit atualiza a funçao handleDrop para atualizar a ordem das etapas de acordo com o tipo de processo.

--- src/admin/components/ProcessTypeManager/ProcessTypeList.js
+++ src/admin/components/ProcessTypeManager/ProcessTypeList.js
@@ -14,23 +14,18 @@ const ProcessTypeList = ({
 
   useEffect(() => {
     const initialStepsState = processTypes.reduce((acc, type) => {
-      const steps = processSteps
-        .filter(step => {
+      acc[type.id] = processSteps
+        .filter((step) => {
           const stepTypes = Array.isArray(step.process_type) ? step.process_type : [step.process_type];
           return stepTypes.includes(type.id);
         })
-        .sort((a, b) => a.step_order - b.step_order);
-
-      acc[type.id] = steps;
-
+        .sort((a, b) => (a.step_order[type.id] || 0) - (b.step_order[type.id] || 0));
       return acc;
     }, {});
-
     console.log('initialStepsState:', initialStepsState);
     setStepsState(initialStepsState);
   }, [processTypes, processSteps]);
 
-  
   const handleDragStart = (event, typeId, stepId) => {
     event.dataTransfer.setData('typeId', typeId.toString());
     event.dataTransfer.setData('stepId', stepId.toString());
@@ -38,143 +33,145 @@ const ProcessTypeList = ({
 
   const handleDrop = async (event, dropIndex, typeId) => {
     event.preventDefault();
+    const draggedTypeId = event.dataTransfer.getData('typeId');
     const draggedStepId = event.dataTransfer.getData('stepId');
-  
-    const updatedSteps = [...stepsState[typeId]];
+
+    if (!stepsState[draggedTypeId]) {
+        console.error(`Steps for typeId ${draggedTypeId} not found.`);
+        return;
+    }
+
+    const updatedSteps = [...stepsState[draggedTypeId]];
     const draggedStepIndex = updatedSteps.findIndex(step => step.id === parseInt(draggedStepId, 10));
     const [draggedStep] = updatedSteps.splice(draggedStepIndex, 1);
     updatedSteps.splice(dropIndex, 0, draggedStep);
-  
-    
+
     setStepsState(prevState => ({
-      ...prevState,
-      [typeId]: updatedSteps,
+        ...prevState,
+        [draggedTypeId]: updatedSteps,
     }));
-  
+
     try {
-      await Promise.all(updatedSteps.map((step, index) => 
-        apiFetch({
-          path: `/wp/v2/process_step/${step.id}`,
-          method: 'PUT',
-          data: { step_order: index },
-        })
-        ));
+        // Atualize a nova ordem no banco de dados
+        await Promise.all(updatedSteps.map((step, index) => {
+            const stepOrder = { ...step.step_order, [typeId]: index };
+            return apiFetch({
+                path: `/wp/v2/process_step/${step.id}`,
+                method: 'PUT',
+                data: { step_order: stepOrder },
+            });
+        }));
         console.log('Ordem das etapas atualizada no banco de dados');
-    }   catch (error) {
+    } catch (error) {
         console.error('Erro ao salvar a ordem das etapas:', error);
     }
-  };
+};
+
 
   const handleDragOver = (event) => {
     event.preventDefault();
   };
 
 
-  return (
+return (
     <Panel>
-        <PanelHeader>Existing Process Types</PanelHeader>
-        <PanelRow>
-            {processTypes.length > 0 ? (
-                <div className="card-container">
-                {processTypes.map((type) => {
-                    const steps = stepsState[type.id] || [];
+      <PanelHeader>Existing Process Types</PanelHeader>
+      <PanelRow>
+        {processTypes.length > 0 ? (
+          <div className="card-container">
+            {processTypes.map((type) => {
+              const steps = stepsState[type.id] || [];
 
-                    return (
-                    <Card key={type.id}>
-                        <CardHeader>
-                        <h4 className="card-title">{type.title.rendered}</h4>
-                        </CardHeader>
-                        <CardBody>
-                        <dl className="description-list">
-                            <div className="list-item">
-                            <dt>Description:</dt>
-                            <dd>
-                                {type.description ? type.description.split('\n').map((item, key) => ( <span key={key}>{item}<br /></span> )) : "-"}
-                            </dd>
-                            </div>
-                        </dl>
+              return (
+                <Card key={type.id}>
+                  <CardHeader>
+                    <h4 className="card-title">{type.title.rendered}</h4>
+                  </CardHeader>
+                  <CardBody>
+                    <dl className="description-list">
+                      <div className="list-item">
+                        <dt>Description:</dt>
+                        <dd>
+                          {type.description ? type.description.split('\n').map((item, key) => (
+                            <span key={key}>{item}<br /></span>
+                          )) : "-"}
+                        </dd>
+                      </div>
+                    </dl>
 
-                        <p className={type.accept_attachments ? "check true" : "check false"}>
-                            {!type.accept_attachments && (
-                            <span className="visually-hidden">Not</span>
-                            )}{" "}
-                            Accept attachments
-                        </p>
-                        <p className={type.accept_tainacan_items ? "check true" : "check false"}>
-                            {!type.accept_tainacan_items && (
-                            <span className="visually-hidden">Not</span>
-                            )}{" "}
-                            Accept Tainacan items
-                        </p>
-                        <p className={type.generate_tainacan_items ? "check true" : "check false"}>
-                            {!type.generate_tainacan_items && (
-                            <span className="visually-hidden">Not</span>
-                            )}{" "}
-                            Generate Tainacan items
-                        </p>
+                    <p className={type.accept_attachments ? "check true" : "check false"}>
+                      {!type.accept_attachments && (
+                        <span className="visually-hidden">Not</span>
+                      )}{" "}
+                      Accept attachments
+                    </p>
+                    <p className={type.accept_tainacan_items ? "check true" : "check false"}>
+                      {!type.accept_tainacan_items && (
+                        <span className="visually-hidden">Not</span>
+                      )}{" "}
+                      Accept Tainacan items
+                    </p>
+                    <p className={type.generate_tainacan_items ? "check true" : "check false"}>
+                      {!type.generate_tainacan_items && (
+                        <span className="visually-hidden">Not</span>
+                      )}{" "}
+                      Generate Tainacan items
+                    </p>
 
-                        {steps.length > 0 && (
-                            <>
-                            <hr />
-                            <h5>Steps</h5>
-                            <ol className="list-group">
-                                {steps.map((step, index) => (
-                                <Draggable
-                                    key={step.id}
-                                    elementId={`step-${step.id}`}
-                                    appendToOwnerDocument={true}
-                                    transferData={{ typeId: type.id, stepId: step.id }}
-                                    onDragStart={(event) => handleDragStart(event, type.id, step.id)}
-                                >
-                                    {({ onDraggableStart, onDraggableEnd }) => (
-                                    <li
-                                        className="list-group-item"
-                                        id={`step-${step.id}`}
-                                        draggable="true"
-                                        onDragOver={handleDragOver}
-                                        onDrop={(event) => handleDrop(event, index, type.id)}
-                                        onDragStart={(event) => handleDragStart(event, type.id, step.id)}
-                                    >
-                                        {step.title.rendered}
-                                        <Tooltip text="Delete Step">
-                                        <Button
-                                            isDestructive
-                                            icon={<Icon icon={trash} />}
-                                            onClick={() => onDeleteStep(step.id)}
-                                        />
-                                        </Tooltip>
-                                    </li>
-                                    )}
-                                </Draggable>
-                                ))}
-                            </ol>
-                            </>
-                        )}
-                        </CardBody>
-                        <CardFooter>
-                        <Tooltip text="Edit">
-                            <Button
-                            icon={<Icon icon={edit} />}
-                            onClick={() => onEdit(type)}
-                            />
-                        </Tooltip>
-                        <Tooltip text="Delete">
-                            <Button
-                            icon={<Icon icon={trash} />}
-                            onClick={() => onDelete(type.id)}
-                            />
-                        </Tooltip>
-                        </CardFooter>
-                    </Card>
-                    );
-                })}
-                </div>
-            ) : (
-                <Notice isDismissible={false} status="warning">No existing processes types.</Notice>
-            )}
-        </PanelRow>
+                    {steps.length > 0 && (
+                      <>
+                        <hr />
+                        <h5>Steps</h5>
+                        <ol className="list-group">
+                          {steps.map((step, index) => (
+                            <li
+                              key={`${step.id}-${index}`}
+                              className="list-group-item"
+                              id={`step-${step.id}-${index}`}
+                              draggable="true"
+                              onDragOver={handleDragOver}
+                              onDrop={(event) => handleDrop(event, index, type.id)}
+                              onDragStart={(event) => handleDragStart(event, type.id, step.id)}
+                            >
+                              {step.title.rendered}
+                              <Tooltip text="Delete Step">
+                                <Button
+                                  isDestructive
+                                  icon={<Icon icon={trash} />}
+                                  onClick={() => onDeleteStep(step.id, type.id)}
+                                />
+                              </Tooltip>
+                            </li>
+                          ))}
+                        </ol>
+                      </>
+                    )}
+                  </CardBody>
+                  <CardFooter>
+                    <Tooltip text="Edit">
+                      <Button
+                        icon={<Icon icon={edit} />}
+                        onClick={() => onEdit(type)}
+                      />
+                    </Tooltip>
+                    <Tooltip text="Delete">
+                      <Button
+                        icon={<Icon icon={trash} />}
+                        onClick={() => onDelete(type.id)}
+                      />
+                    </Tooltip>
+                  </CardFooter>
+                </Card>
+              );
+            })}
+          </div>
+        ) : (
+          <Notice isDismissible={false} status="warning">No existing process types.</Notice>
+        )}
+      </PanelRow>
     </Panel>
-);
+  );
+  
 };
 
 export default ProcessTypeList;
