8a69e3f73d3428e283d2024c8b5446c4efa6f758
Fábio Rocha <fabio.gomes@ifrn.edu.br>
2024-07-30 11:58:35 -0300

    Styling multiple selection

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-element', 'wp-primitives'), 'version' => '8721c45a177b4de0003d');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-element', 'wp-primitives'), 'version' => '104a5ad378c42c6d7159');
--- src/admin/components/ProcessTypeManager/ProcessStepForm.js
+++ src/admin/components/ProcessTypeManager/ProcessStepForm.js
@@ -41,15 +41,24 @@ const ProcessStepForm = ({ processTypes, processSteps, onAddStep }) => {
     };
 
     return (
-        <PanelBody title="Add Process Step" initialOpen={true}>
+        <PanelBody title="Add a Process Type Step" initialOpen={true}>
             <PanelRow>
                 {notice && (
                     <Notice status={notice.status} isDismissible onRemove={() => setNotice(null)}>
                         {notice.message}
                     </Notice>
                 )}
+                <SelectControl
+                    label="Select Process Type"
+                    value={selectedProcessType}
+                    options={[
+                        { label: 'Select a process type...', value: '' },
+                        ...processTypes.map(type => ({ label: type.title.rendered, value: type.id }))
+                    ]}
+                    onChange={(value) => setSelectedProcessType(value)}
+                />
                 <ComboboxControl
-                    label="Select Step"
+                    label="Select one or more Steps"
                     value={stepInputValue}
                     options={processSteps
                         .filter(step => !selectedSteps.includes(step.title.rendered))
@@ -69,15 +78,6 @@ const ProcessStepForm = ({ processTypes, processSteps, onAddStep }) => {
                         </div>
                     ))}
                 </div>
-                <SelectControl
-                    label="Select Process Type"
-                    value={selectedProcessType}
-                    options={[
-                        { label: 'Select a process type...', value: '' },
-                        ...processTypes.map(type => ({ label: type.title.rendered, value: type.id }))
-                    ]}
-                    onChange={(value) => setSelectedProcessType(value)}
-                />
                 <Button isSecondary onClick={handleAddStep}>
                     Add Process Step
                 </Button>
