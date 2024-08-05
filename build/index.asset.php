35f98ab8646c6df4e66d9569aa0ad141ac364b86
Fábio Rocha <fabio.gomes@ifrn.edu.br>
2024-08-05 16:24:25 -0300

    Updating metadata list

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-primitives'), 'version' => '4f70202aafd9c1079758');
-=======
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-primitives'), 'version' => 'c154e9b6ab387af4072c');
->>>>>>> 38d882fbc14e63ed41e58ae235660058b0454a47
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-primitives'), 'version' => '59957d1b61fb35546b0f');
