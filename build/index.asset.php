17d81f9808f9d038a185576db9e92ac3d633c180
Douglas Everbero <dougas@3xweb.site>
2024-08-05 13:04:37 -0300

    merge build/index.js build/index.js.map

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-primitives'), 'version' => 'af61ff62a59d28ab1800');
-=======
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-primitives'), 'version' => 'c154e9b6ab387af4072c');
->>>>>>> front
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-primitives'), 'version' => '19d2aa37fe4c2a8cd43e');
