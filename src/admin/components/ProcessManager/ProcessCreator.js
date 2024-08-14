64900c919577275af081ae51d71881ae98c5c95f
anaclrra <anaclarab826@gmail.com>
2024-08-14 15:45:58 -0300

    refactor: adiciona campo Access Level Este commit modifica a funçao handleSaveProcess p/ lidar com a criacao e atualizaçao do processo

--- src/admin/components/ProcessManager/ProcessCreator.js
+++ src/admin/components/ProcessManager/ProcessCreator.js
@@ -1,73 +1,119 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Button, SelectControl, TextControl, Panel, PanelHeader, PanelBody, PanelRow, Notice } from '@wordpress/components';
 import apiFetch from '@wordpress/api-fetch';
 
-const ProcessCreator = ({ processTypes, onProcessCreated }) => {
+const ProcessCreator = ({ processTypes, onProcessSaved, editingProcess, onCancel }) => {
     const [newProcessTitle, setNewProcessTitle] = useState('');
     const [newProcessType, setNewProcessType] = useState('');
+    const [accessLevel, setAccessLevel] = useState('public');
     const [notice, setNotice] = useState(null);
+    
+    const formRef = useRef(null);
 
-    const handleCreateProcess = async () => {
+    useEffect(() => {
+        if (editingProcess) {
+            console.log( editingProcess )
+            setAccessLevel(editingProcess.meta.access_level);
+            setNewProcessTitle(editingProcess.title.rendered);
+            setNewProcessType(editingProcess.meta.process_type);
+
+            if (formRef.current) {
+                formRef.current.scrollIntoView({ behavior: 'smooth', block: 'start' });
+            }
+        }
+    }, [editingProcess]);
+
+    const handleSaveProcess = async () => {
         if (!newProcessTitle || !newProcessType) {
             setNotice({ status: 'error', message: 'Please provide a title and select a process type.' });
             return;
         }
-
+    
         const selectedProcessType = processTypes.find(type => type.id === parseInt(newProcessType));
-
+    
         if (!selectedProcessType) {
             setNotice({ status: 'error', message: 'Invalid process type selected.' });
             return;
         }
-        
+    
         const newProcess = {
             title: newProcessTitle,
             status: 'publish',
             type: 'process_obatala',
-            meta: {
-                process_type: selectedProcessType.id,
-                current_stage: null,
-            }
         };
-
+    
         try {
-            const savedProcess = await apiFetch({
-                path: `/obatala/v1/process_obatala`,
-                method: 'POST',
-                data: newProcess
-            });
-
+            let savedProcess;
+    
+            if (editingProcess) {
+                // Atualiza o processo
+                savedProcess = await apiFetch({
+                    path: `/obatala/v1/process_obatala/${editingProcess.id}`,
+                    method: 'POST',
+                    data: { ...newProcess }
+                });
+            } else {
+                // Cria o processo
+                savedProcess = await apiFetch({
+                    path: `/obatala/v1/process_obatala`,
+                    method: 'POST',
+                    data: newProcess
+                });
+            }
+    
             console.log(savedProcess);
-
+    
             const stepOrder = selectedProcessType.meta.step_order || [];
-            const metaFieldsPromises = stepOrder.map(stepId => apiFetch({ path: `/obatala/v1/process_step/${stepId}/meta` }));
-
+            const metaFieldsPromises = stepOrder.map(stepId => 
+                apiFetch({ path: `/obatala/v1/process_step/${stepId}/meta` })
+            );
+    
             const metaFieldsResults = await Promise.all(metaFieldsPromises);
-
+    
             const stepOrderWithMeta = stepOrder.map((stepId, index) => ({
                 step_id: stepId,
                 meta_fields: metaFieldsResults[index]
             }));
-
+    
+            const metaUpdateData = {
+                step_order: stepOrderWithMeta,
+                process_type: selectedProcessType.id,
+                current_stage: 0,
+                access_level: accessLevel
+            };
+    
+            // Atualiza o meta para o processo 
             await apiFetch({
                 path: `/obatala/v1/process_obatala/${savedProcess.id}/meta`,
                 method: 'POST',
-                data: { step_order: stepOrderWithMeta }
+                data: metaUpdateData
             });
-
-            onProcessCreated(savedProcess);
+    
+            // Atualiza o objeto savedProcess com os metas
+            savedProcess.meta = metaUpdateData;
+    
+            onProcessSaved(savedProcess);
             setNewProcessTitle('');
             setNewProcessType('');
-            setNotice({ status: 'success', message: 'Process created successfully.' });
+            setAccessLevel('public');
+            setNotice({ status: 'success', message: editingProcess ? 'Process updated successfully.' : 'Process created successfully.' });
         } catch (error) {
             console.error('Error creating process:', error);
             setNotice({ status: 'error', message: 'Error creating process.' });
         }
     };
+    
+
+    const handleCancel = () => {
+        onCancel();
+        setNewProcessTitle('');
+        setNewProcessType('');
+        setAccessLevel('public');
+    };
 
     return (
-        <Panel>
-            <PanelHeader>Create Process</PanelHeader>
+        <Panel ref={formRef}>
+            <PanelHeader>{editingProcess ? 'Edit Process' : 'Create Process'}</PanelHeader>
             {notice && (
                 <Notice status={notice.status} isDismissible onRemove={() => setNotice(null)}>
                     {notice.message}
@@ -79,6 +125,7 @@ const ProcessCreator = ({ processTypes, onProcessCreated }) => {
                         label="Process Title"
                         value={newProcessTitle}
                         onChange={(value) => setNewProcessTitle(value)}
+                        disabled={!!editingProcess}
                     />
                     <SelectControl
                         label="Process Type"
@@ -88,9 +135,24 @@ const ProcessCreator = ({ processTypes, onProcessCreated }) => {
                             ...processTypes.map(type => ({ label: type.title.rendered, value: type.id }))
                         ]}
                         onChange={(value) => setNewProcessType(value)}
+                        disabled={!!editingProcess}
                     />
-                    <Button isPrimary onClick={handleCreateProcess}>Create Process</Button>
+
+                    <SelectControl
+                        label="Access Level"
+                        value={accessLevel}
+                        options={[
+                            { label: 'Public', value: 'public' },
+                            { label: 'Private', value: 'private' }
+                        ]}
+                        onChange={(value) => setAccessLevel(value)}
+                    />
                 </PanelRow>
+                <PanelRow>
+                    <Button isPrimary onClick={handleSaveProcess}>Save</Button>
+                    <Button isSecondary onClick={handleCancel}>Cancel</Button>
+                </PanelRow>
+                    
             </PanelBody>
         </Panel>
     );
