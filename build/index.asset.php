e442aadd196a535d72cd81d1850b4849a6ab4bf0
Fábio Rocha <fabio.gomes@ifrn.edu.br>
2024-09-06 17:03:54 -0300

    Ajustes de usabilidade para form de processo

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-primitives'), 'version' => 'e7f8bdadcc4c488639f7');
-=======
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-primitives'), 'version' => 'dc6ac52ff023dd2a416a');
->>>>>>> 9b4c34865a57192dab5a97588f779ee706aadd56
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-primitives'), 'version' => '972e4457389d501d1917');
