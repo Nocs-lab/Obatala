0f38772d1c422038933c3cc3614bf65418ee3c52
anaclrra <anaclarab826@gmail.com>
2024-09-17 15:15:35 -0300

    modifica componente para exibir, etapas e campos com base no novo modelo de tipo de processo Além de salvar os valores dos meta campos de cada etapa

--- src/admin/components/ProcessViewer.js
+++ src/admin/components/ProcessViewer.js
@@ -5,14 +5,13 @@ import { fetchProcessTypes } from '../api/apiRequests';
 import MetroNavigation from './ProcessManager/MetroNavigation';
 import MetaFieldInputs from './ProcessManager/MetaFieldInputs';
 import CommentForm from './ProcessManager/CommentForm';
-import { check, Icon } from '@wordpress/icons';
 
 const ProcessViewer = () => {
     const [process, setProcess] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState(null);
     const [currentStep, setCurrentStep] = useState(0);
-    const [steps, setSteps] = useState([]);
+    const [flowNodes, setFlowNodes] = useState([]);
     const [filteredProcessType, setFilteredProcessType] = useState(null);
     const [processTypes, setProcessTypes] = useState([]);
     const [submittedSteps, setSubmittedSteps] = useState({});
@@ -44,10 +43,10 @@ const ProcessViewer = () => {
     }, [processTypes]);
 
     useEffect(() => {
-        if (process) {
+        if (process && filteredProcessType) {
             fetchSteps();
         }
-    }, [process]);
+    }, [process, filteredProcessType]);
 
     const fetchProcess = (processId) => {
         setIsLoading(true);
@@ -64,14 +63,18 @@ const ProcessViewer = () => {
     };
 
     const fetchSteps = async () => {
-        try {
-            const stepsData = await apiFetch({ path: `/obatala/v1/process_step` });
-            setSteps(stepsData);
-        } catch (error) {
-            console.error('Error fetching steps:', error);
-            setError('Error fetching steps details.');
-        }
-    };
+        setIsLoading(true);
+        apiFetch({ path: `/obatala/v1/process_type/${filteredProcessType.id}?_embed` })
+            .then(data => {
+                setFlowNodes(data.meta.flowData);
+                setIsLoading(false);
+            })
+            .catch(error => {
+                console.error('Error fetching steps:', error);
+                setError('Error fetching steps details.');
+                setIsLoading(false);
+            });
+    }; 
 
     const fetchLoadProcess = () => {
         setIsLoading(true);
@@ -103,24 +106,90 @@ const ProcessViewer = () => {
 
     const handleFieldChange = (fieldId, newValue) => {
         setFormValues(prevValues => {
-            const updatedValues = { ...prevValues[currentStep], [fieldId]: newValue };
-            return {
+            const updatedValues = {
                 ...prevValues,
-                [currentStep]: updatedValues, 
+                [currentStep]: {
+                    ...prevValues[currentStep],
+                    [fieldId]: newValue
+                }
             };
+            return updatedValues;
+            
         });
-        
         setIsSubmitEnabled(formValues);
-    }; 
+    };
     
-    const handleSubmit = () => {
-        setSubmittedSteps(prev => ({
-            ...prev,
-            [currentStep]: true,
-        }));
-        setIsSubmitEnabled(false);
-        console.log('Form values:', formValues);
+    const handleSubmit = async () => {
+        const updatedFlowData = {
+            ...flowNodes,
+            nodes: flowNodes.nodes.map(node => {
+                if (node.id === orderedSteps[currentStep].id) {
+                    return {
+                        ...node,
+                        fields: node.fields.map(field => {
+                            const newValue = formValues[currentStep]?.[field.id] || field.value;
+                            return {
+                                ...field,
+                                value: newValue
+                            };
+                        })
+                    };
+                }
+                return node;
+            })
+        };
 
+        try {
+            const result = await apiFetch({
+                path: `/obatala/v1/process_type/${filteredProcessType.id}/meta`,
+                method: 'PUT',
+                data: { flowData: updatedFlowData }
+            });
+            if(result){
+                setSubmittedSteps(prev => ({
+                    ...prev,
+                    [currentStep]: true,
+                }));
+            }
+        } catch (error) {
+            console.error('Error saving metadata: ', error);
+            setError('Error saving metadata.');
+
+        }
+    };
+
+
+    
+    const getOrderedSteps = () => {
+        if (flowNodes && flowNodes.nodes){
+            const { edges, nodes } = flowNodes;
+            const nodeMap = new Map(nodes.map(node => [node.id, node]));
+            const sources = new Set(edges.map(edge => edge.source));
+            const targets = new Set(edges.map(edge => edge.target));
+
+            const initialStep = nodes.filter(node => sources.has(node.id) && !targets.has(node.id));
+            
+            const orderedSteps = [];
+            const visited = new Set();
+    
+            const visit = (nodeId) => {
+                if (visited.has(nodeId)) return;
+                visited.add(nodeId);
+                const node = nodeMap.get(nodeId);
+                if (node) {
+                    orderedSteps.push(node);
+                    edges
+                        .filter(edge => edge.source === nodeId)
+                        .forEach(edge => visit(edge.target));
+                }
+            };
+    
+            initialStep.forEach(node => visit(node.id));
+    
+            return orderedSteps;
+
+        }
+        return [];
     };
 
     if (isLoading) {
@@ -135,13 +204,8 @@ const ProcessViewer = () => {
         return <Notice status="warning" isDismissible={false}>No process found.</Notice>;
     }
 
-    const orderedSteps = process.meta.step_order.map(order => {
-        const step = steps.find(s => s.id === order.step_id);
-        return { ...order, title: step ? step.title.rendered : 'Unknown Title', meta_fields: order.meta_fields || [] };
-    });
-
-    const options = orderedSteps.map(step => ({ label: step.title, value: step.step_id }));
-
+    const orderedSteps = getOrderedSteps();
+    const options = orderedSteps.map(step => ({ label: step.stageName, value: step.id }));
     return (
         <div>
             <span className="brand"><strong>Obatala</strong> Curatorial Process Viewer</span>
@@ -164,19 +228,21 @@ const ProcessViewer = () => {
                     {orderedSteps.length > 0 && orderedSteps[currentStep] ? (
                         <Panel key={`${orderedSteps[currentStep].step_id}-${currentStep}`}>
                             <PanelHeader>
-                                <h3>{`${orderedSteps[currentStep].title}`}</h3>
+                                <h3>{`${orderedSteps[currentStep].stageName}`}</h3>
                                 <span className="badge default ms-auto">Setor: Recepção</span>
                             </PanelHeader>
                             <PanelBody>
                                 <PanelRow>
-                                    <form className="centered" action="">
+                                    
+                                    {orderedSteps[currentStep].fields.length > 0 ? (
+                                        <form className="centered" action="">
                                         <ul className="meta-fields-list">
-                                            {Array.isArray(orderedSteps[currentStep].meta_fields) ? orderedSteps[currentStep].meta_fields.map((field, idx) => (
-                                                <li key={`${orderedSteps[currentStep].step_id}-meta-${idx}`} className="meta-field-item">
+                                            {Array.isArray(orderedSteps[currentStep].fields) ? orderedSteps[currentStep].fields.map((field, idx) => (
+                                                <li key={`${orderedSteps[currentStep].id}-meta-${idx}`} className="meta-field-item">
                                                     <MetaFieldInputs 
                                                         field={field} 
-                                                        fieldId={idx}  
-                                                        initalValue={formValues[currentStep]?.[idx] || ''}
+                                                        fieldId={field.id}  
+                                                        initalValue={formValues[currentStep]?.[field.id] || ''}
                                                         isEditable={!submittedSteps[currentStep]} 
                                                         onFieldChange={handleFieldChange} 
                                                     />
@@ -187,11 +253,15 @@ const ProcessViewer = () => {
                                             <Button
                                                 isPrimary
                                                 onClick={handleSubmit}
-                                                disabled={!isSubmitEnabled || submittedSteps[currentStep]}
+                                                disabled={!isSubmitEnabled[currentStep] || submittedSteps[currentStep]}
                                             >Submit
                                             </Button>
                                         </div>
                                     </form>
+                                    ) : (
+                                        <Notice status="warning" isDismissible={false}>No fields found for this Step.</Notice>
+                                    )}
+                                    
                                 </PanelRow>
                                 <footer>Última atualização em 21/10/2024 por João da Silva</footer>
                             </PanelBody>
@@ -212,3 +282,5 @@ const ProcessViewer = () => {
 };
 
 export default ProcessViewer;
+
+
