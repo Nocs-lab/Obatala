1d10c7ac59a84810badd648cb6c2896c40609658
Douglas Everbero <douglas@3xweb.site>
2024-09-25 22:18:04 -0300

    chore: remove build files from git this commit removes the build files from git and adds them to the .gitignore file this is donte to avoid commom conflicts and to keep the repository clean

--- build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-primitives'), 'version' => '1a6119a17ef08ed48ab2');
