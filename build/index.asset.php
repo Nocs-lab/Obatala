b37549476530b82c447dc372b8824687db9988d9
Douglas Everbero <douglas@3xweb.site>
2024-10-17 09:16:45 -0300

    chore: update release workflow

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-primitives'), 'version' => 'c34e16af3afb3194efe4');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-primitives'), 'version' => '0a925c8ad15b86295c87');
