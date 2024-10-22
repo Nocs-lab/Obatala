2ea51fb7b4ed13f5f5f97eaaf2bcf58b7edb6a20
anaclrra <anaclarab826@gmail.com>
2024-10-22 20:13:13 -0300

    refac: adiciona coluna 'Number Of Users e botão para gerenciar usuários'

--- src/admin/components/SectorManager/SectorList.js
+++ src/admin/components/SectorManager/SectorList.js
@@ -1,11 +1,36 @@
-import React, { useMemo } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
+import apiFetch from "@wordpress/api-fetch";
 import { Button, ButtonGroup, Icon, Tooltip, Panel, PanelHeader, PanelRow, Notice } from '@wordpress/components';
-import { edit, trash} from '@wordpress/icons';
+import { edit, trash, people} from '@wordpress/icons';
 
 
-const SectorList = ({sectors, onEdit, onDelete}) => {
+const SectorList = ({sectors, onEdit, onDelete, onAddUser}) => {
     const filteredSectors = useMemo(() => sectors, [sectors]);
+    const [usersSectors, setUsersSectors] = useState({});
     
+    useEffect(() => {
+        if (filteredSectors.length > 0) {
+            loadSectorUsers();
+        }
+    }, [filteredSectors]);
+
+    const loadSectorUsers = async () => {
+        const updatedSectors = await Promise.all(
+            filteredSectors.map(async (sector) => {
+                console.log(sector.id)
+                try {
+                    const data = await apiFetch({ path: `/obatala/v1/sector_obatala/${sector.id}/users` })
+                    return {...sector, userCount: data.length}
+                    
+                } catch (error) {
+                    console.error(`Erro ao buscar usuários do setor ${sector.id}:`, error);
+                    return { ...sector, userCount: 0 };
+                }
+            })
+        )
+        setUsersSectors(updatedSectors);
+    } 
+    console.log(usersSectors)
     return (
         <Panel>
             <PanelHeader>
@@ -13,23 +38,25 @@ const SectorList = ({sectors, onEdit, onDelete}) => {
                 <span className="badge">{filteredSectors.length}</span>
             </PanelHeader>
             <PanelRow>
-                {filteredSectors.length > 0 ? (
+                {usersSectors.length > 0 ? (
                     <table className="wp-list-table widefat fixed striped">
                         <thead>
                             <tr>
                                 <th>Title</th>
                                 <th>Description</th>
+                                <th>Number of users</th>
                                 <th>Status</th>
                                 <th>Actions</th>
                                 
                             </tr>
                         </thead>
                         <tbody>
-                            {filteredSectors.map(sector => {
+                            {usersSectors.map(sector => {
                                 return (
                                     <tr key={sector.id}>
                                         <td>{sector.name}</td>
                                         <td>{sector.description}</td>
+                                        <td>{sector?.userCount}</td>
                                         <td><span className={`badge ${sector.status == 'Active' ? 'success' : 'error'}`}>{sector.status}</span></td>
                                         <td>
                                             <ButtonGroup>
@@ -42,7 +69,13 @@ const SectorList = ({sectors, onEdit, onDelete}) => {
                                                 <Tooltip text="Delete">
                                                     <Button
                                                         icon={<Icon icon={trash} />}
-                                                        onClick={() => onDelete(sector.id)}
+                                                        onClick={() => onDelete(sector)}
+                                                    />
+                                                </Tooltip>
+                                                <Tooltip text="Users">
+                                                    <Button
+                                                        icon={<Icon icon={people} />}
+                                                        onClick={() => onAddUser(sector)}
                                                     />
                                                 </Tooltip>
                                             </ButtonGroup>
