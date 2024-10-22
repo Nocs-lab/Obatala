6ffcf5f9cc4852b7bdfb23160cd50ba6870b2d93
anaclrra <anaclarab826@gmail.com>
2024-10-22 20:34:42 -0300

    refac: Adiciona alteraçoes removidas

--- classes/Api/ProcessApi.php
+++ classes/Api/ProcessApi.php
@@ -53,6 +53,13 @@ class ProcessApi extends ObatalaAPI {
             ]
         ]);
 
+        // Route to get meta fields
+        $this->add_route('process_obatala/(?P<id>\d+)/meta', [
+            'methods' => 'GET',
+            'callback' => [$this, 'get_meta'],
+            'permission_callback' => '__return_true',
+        ]);
+        
         // Route to update multiple meta fields
         $this->add_route('process_obatala/(?P<id>\d+)/meta', [
             'methods' => 'POST',
@@ -129,7 +136,19 @@ class ProcessApi extends ObatalaAPI {
         $process_type = (int) $request['process_type'];
         return update_post_meta($post_id, 'process_type', $process_type);
     }
+    public function get_meta($request) {
+        $post_id = (int) $request['id'];
+    
+        $stageData = maybe_unserialize(get_post_meta($post_id, 'stageData', true));
+        $submittedStages = maybe_unserialize(get_post_meta($post_id, 'submittedStages', true));
 
+        $response = array(
+            'stageData' => $stageData,
+            'submittedStages' => $submittedStages,
+        );
+    
+        return rest_ensure_response($response);
+    }
     public function update_meta($request) {
         $post_id = (int) $request['id'];
         $meta = $request->get_json_params();
