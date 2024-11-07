04aac94a61674aee5e341fc81746e3ea76229de3
Fábio Rocha <fabio.gomes@ifrn.edu.br>
2024-11-06 21:37:03 -0300

    Melhorias botoes de fields de steps

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-primitives'), 'version' => '0997846aca448265ed50');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-primitives'), 'version' => 'a16a23ec6fbbc168aa16');
--- src/admin/components/FlowEditor/components/dragables/SortableField.js
+++ src/admin/components/FlowEditor/components/dragables/SortableField.js
@@ -7,7 +7,7 @@ import { DatePickerControls } from "../inputControls/DatePickerControls";
 import { FileUploadControls } from "../inputControls/FileUploadControls";
 import { SelectRadioControls } from "../inputControls/SelectRadioControls";
 import { Button, ButtonGroup, Icon, Tooltip } from "@wordpress/components";
-import { edit, trash } from "@wordpress/icons";
+import { edit, dragHandle, trash } from "@wordpress/icons";
 import { useDrawer } from "../../context/DrawerContext";
 import LabelWithIcon from "../inputControls/LabelWithIcon";
 import { useFlowContext } from "../../context/FlowContext";
@@ -104,45 +104,43 @@ const SortableField = ({ id, nodeId, title, type, config }) => {
     }
   };
 
-  const style = {
-    transform: CSS.Transform.toString(transform),
-    transition: transition || "transform 250ms ease",
-    opacity: isDragging ? 0.8 : 1,
-    cursor: isDragging ? "grabbing" : "pointer",
-    boxShadow: isDragging ? "0 4px 8px rgba(0, 0, 0, 0.1)" : "none",
-  };
+    const style = {
+        transform: CSS.Transform.toString(transform),
+        transition: transition || "transform 250ms ease",
+        opacity: isDragging ? 0.8 : 1,
+        cursor: isDragging ? "grabbing" : "pointer",
+        boxShadow: isDragging ? "0 4px 8px rgba(0, 0, 0, 0.1)" : "none",
+    };
 
     return (
         <li ref={setNodeRef} style={style} {...attributes}>
             <LabelWithIcon label={config ? config.label : label} type={type} />
             <ButtonGroup>
                 <Tooltip text="Edit">
-                <Button
-                    icon={<Icon icon={edit}/>}
-                    onClick={() => toggleDrawer(renderFieldControls())}
-                    size="small"
-                    />
+                    <Button
+                        icon={<Icon icon={edit}/>}
+                        onClick={() => toggleDrawer(renderFieldControls())}
+                        size="small"
+                        />
                 </Tooltip>
                 {/* Passa o nodeId e o id do campo para remover */}
                 <Tooltip text="Remove">
-                <Button
-                    icon={<Icon icon={trash}/>}
-                    onClick={() => removeFieldFromNode(nodeId, id)} // Aqui você passa o nodeId e o id do campo
-                    size="small"
-                    />
+                    <Button
+                        icon={<Icon icon={trash}/>}
+                        onClick={() => removeFieldFromNode(nodeId, id)} // Aqui você passa o nodeId e o id do campo
+                        size="small"
+                        />
                 </Tooltip>
-            </ButtonGroup>
-
-            {/* Drag handle */}
-            <div
-                {...listeners} // Listeners aplicados ao ícone de arraste
-                className="flow-grabber"
-                onClick={(e) => e.stopPropagation()} // Evita a expansão ao clicar no drag handle
-            >
-                <span role="img" aria-label="drag">
-                ⠿
-                </span>
-            </div>
+                {/* Drag handle */}
+                <Tooltip text="Reorder">
+                    <Button
+                        {...listeners} // Listeners aplicados ao ícone de arraste
+                        icon={<Icon icon={dragHandle}/>}
+                        onClick={(e) => e.stopPropagation()} // Evita a expansão ao clicar no drag handle
+                        size="small"
+                        />
+                </Tooltip>
+            </ButtonGroup>            
         </li>
     );
 };
