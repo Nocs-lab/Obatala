f86c3d0619528b0c2e5c4634e206774177db1a9c
Joao Paulo da Silva Monteiro <jpcabal2@hotmail.com>
2024-11-18 22:00:36 -0300

    apagar

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-primitives'), 'version' => 'a16a23ec6fbbc168aa16');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-primitives'), 'version' => '769efa5fe02c6697b57d');
