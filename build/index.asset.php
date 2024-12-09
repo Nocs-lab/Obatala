24cf310900b70fb0b490e4d91531e0e6f9e6d45c
Joao Paulo da Silva Monteiro <jpcabal2@hotmail.com>
2024-12-09 09:54:36 -0300

    raw: comentar linha no CommentForm para ocultar mensagem de erro

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => 'f69d4a0911c392f0f1c4');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => 'e916dc4727703fcc2fcf');
