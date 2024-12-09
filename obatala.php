d6cce444ec7aec18e397938c2a2ea4b5169b2e9c
Nocs Bot 🤖 <info@nocslab.ifrn.edu.br>
2024-12-09 15:07:42 +0000

    chore: update plugin version to v1.2.26

--- obatala.php
+++ obatala.php
@@ -7,7 +7,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 /*
 	Plugin Name: Obatala - Plugin de Gestão de Processos Curatoriais para WordPress
 	Description: Adiciona funcionalidades de gestão de processos curatoriais para o plugin Tainacan
-	Version: 1.2.25
+	Version: 1.2.26
 	Author: Douglas de Araújo
 	Author URI: github.com/everbero
 	License: GPLv2 or later
