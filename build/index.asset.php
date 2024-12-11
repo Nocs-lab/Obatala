978c6d224c2b705a59b067d8479168fd2fb97abe
Fábio Rocha <fabio.gomes@ifrn.edu.br>
2024-12-11 10:43:31 -0300

    bundle

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => 'ad220136bcabbd1d7ea3');
-=======
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => 'e916dc4727703fcc2fcf');
->>>>>>> 680508fbf6277b59ed5beb2ef0ce881066172f5f
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => 'f77ee75c7d11be79163a');
