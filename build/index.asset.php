c0ba265115e22585642aeee0a288e9c05970d164
anaclrra <anaclarab826@gmail.com>
2024-12-17 01:50:52 -0300

    feat: adiciona confirmaçao ao excluir campo da etapa

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => '8c4edb90b99e7e7a47b8');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => '2cd7a1fbfe5c42aeeca5');
