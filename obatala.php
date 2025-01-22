fa700c884cef9e632e6fb68a620d3777ba47312a
Luciano Alexandre de Farias Silva <luciano.cnrn@gmail.com>
2025-01-22 14:44:59 -0300

    Update obatala.php

--- obatala.php
+++ obatala.php
@@ -7,9 +7,8 @@ require_once __DIR__ . '/vendor/autoload.php';
 /*
 	Plugin Name: Obatala - Plugin de Gestão de Processos Curatoriais para WordPress
 	Description: Adiciona funcionalidades de gestão de processos curatoriais para o plugin Tainacan
-	Version: 1.2.32
-	Author: Douglas de Araújo
-	Author URI: github.com/everbero
+	Version: 1.2.33
+	Author: NOCs
 	License: GPLv2 or later
 	Text Domain: obatala
 */
