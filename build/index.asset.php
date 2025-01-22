4e10907a1f1bf5cff145d29d4d86859224cee706
Joao Paulo da Silva Monteiro <jpcabal2@hotmail.com>
2025-01-22 08:22:02 -0300

    raw: atualizar index.asset

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => '7eff7189785489a8def0');
-=======
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => 'a5558d6efc31309f16fb');
->>>>>>> development
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => '01750df0f74460b7fb0d');
