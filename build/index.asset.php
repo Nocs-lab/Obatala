96f323dd3399a73a883b203f66ad60a499bb555c
luciano-alexandre <luciano.cnrn@gmail.com>
2025-01-24 10:22:04 -0300

    Alteração da estrutura do FLow Data para adicionar os dados do NodeCondicional

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => 'e3ccd5b00985cc55c5e0');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => '4b8d6e0fe88012eb2192');
