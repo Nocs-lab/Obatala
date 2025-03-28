6bbab33e63fb67ce10dac7d90b64b5f475992bd7
Fábio Rocha <fabio.gomes@ifrn.edu.br>
2025-03-27 22:49:10 -0300

    .

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => '88fe3b7f1d9d124c1f46');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => '41cd4a5aa38f1a9adb45');
