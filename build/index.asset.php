e26cae76b852081f11d4de86ac08ea5705f43c50
fabio <fabio.gomes@ifrn.edu.br>
2025-05-16 11:22:46 -0300

    Melhorias para impressão

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => '9b1712dd24945562c064');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => '5a0fa307dc3851990978');
