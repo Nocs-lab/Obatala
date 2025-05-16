8151dc9123aeb4bcdb78b067e3cce5d7de5c2e27
fabio <fabio.gomes@ifrn.edu.br>
2025-05-16 10:22:11 -0300

    #289

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => 'e58acf2f3454ace7e437');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => '31df3f0c5cc700409abd');
