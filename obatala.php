24ccfa80f345510f6a45ce680449ec59d1d5f79b
Nocs Bot 🤖 <info@nocslab.ifrn.edu.br>
2026-03-13 17:57:26 +0000

    chore: update plugin version to v1.6.20

--- obatala.php
+++ obatala.php
@@ -15,7 +15,7 @@ namespace Obatala {
 	/*
 		Plugin Name: Obatala - Gestão de Processos Curatoriais
 		Description: Adiciona funcionalidades de gestão de processos curatoriais para o plugin Tainacan
-		Version: 1.6.19
+		Version: 1.6.20
 		Author: NOCs
 		License: GPLv2 or later
 		Text Domain: obatala
