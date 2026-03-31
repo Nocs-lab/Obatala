1d3982560de9ef93846f5c01b7a10beeb483e336
Joao <araujo.nascimento@escolar.ifrn.edu.br>
2026-03-31 10:53:07 -0300

    Criação da primeira versão do relatório de processos

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n', 'wp-primitives'), 'version' => 'cf4833dc589d1cb352ae');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n', 'wp-primitives'), 'version' => '1c77b99ca99f04d598eb');
