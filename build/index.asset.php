bc82a2cb1d4f8e90282bd3c6cdf02b2615d82597
luciano.farias <luciano.cnrn@gmail.com>
2026-05-25 20:14:23 -0300

    parar de versionar build

--- build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n', 'wp-primitives'), 'version' => '16ad83c1ee920ab9a0df');
